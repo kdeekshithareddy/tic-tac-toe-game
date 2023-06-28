2ebda1ce45adf40028bcb0897d95f2ed54167dfc
Deekshitha Reddy <98227586+kdeekshithareddy@users.noreply.github.com>
2023-06-28 13:41:05 +0530

    tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -25,3 +25,34 @@ filemenu.add_command(label="New Game", command=self.new_game)
 filemenu.add_command(label="Quit", command=self.quit)
 menubar.add_cascade(label="Tic-Tac-Toe", menu=filemenu)
 master.config(menu=menubar)
+
+self.new_game()
+
+def new_game(self):
+self.current_player = "X"
+self.board = ["", "", "", "", "", "", "", "", ""]
+for button in self.buttons:
+button.config(text="")
+messagebox.showinfo("New Game", "Let's play Tic Tac Toe!")
+
+def quit(self):
+  self.master.quit()
+
+def on_click(self, index):
+if self.board[index] == "":
+self.board[index] = self.current_player
+self.buttons[index].config(text=self.current_player)
+if self.check_win():
+messagebox.showinfo("Game Over", f"{self.current_player} wins!")
+self.new_game()
+elif self.check_draw():
+messagebox.showinfo("Game Over", "It's a draw!")
+self.new_game()
+else:
+self.current_player = "O" if self.current_player == "X" else "X"
+def check_win(self):
+return (self.board[0] == self.board[1] == self.board[2] != "") or \
+(self.board[3] == self.board[4] == self.board[5] != "") or \
+(self.board[6] == self.board[7] == self.board[8] != "") or \
+(self.board[0] == self.board[3] == self.board[6] != "") or \
+(self.board[1] == self.board[4] == self.board[7] != "") or \
