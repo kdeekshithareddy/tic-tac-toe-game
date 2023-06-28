ed2537c7edd51676f61676fc7b53ecf5878f9c84
Deekshitha Reddy <98227586+kdeekshithareddy@users.noreply.github.com>
2023-06-28 13:35:49 +0530

    tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -9,3 +9,11 @@ master.title("Tic-Tac-Toe")
 # INITIALIZE GAME VARIABLES
 self.current_player = "X"
 self.board = ["", "", "", "", "", "", "", "", ""]
+
+# CREATE GUI ELEMENTS
+self.buttons = []
+for i in range(9):
+button = tk.Button(master, text="", width=3, height=1, command=lambda
+i=i: self.on_click(i))
+button.grid(row=i // 3, column=i % 3)
+self.buttons.append(button)
