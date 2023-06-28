0b026c78628da2c8816b0a7081e4d589dea1920c
Deekshitha Reddy <98227586+kdeekshithareddy@users.noreply.github.com>
2023-06-28 13:35:00 +0530

    tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,11 @@
+import tkinter as tk
+from tkinter import messagebox
 
+class TicTacToeGUI:
+def _init_(self, master):
+self.master = master
+master.title("Tic-Tac-Toe")
+
+# INITIALIZE GAME VARIABLES
+self.current_player = "X"
+self.board = ["", "", "", "", "", "", "", "", ""]
