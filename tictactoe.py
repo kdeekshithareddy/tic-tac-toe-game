e42a6451dc6e617ae3dab1937e9d01fd79ec879a
Deekshitha Reddy <98227586+kdeekshithareddy@users.noreply.github.com>
2023-06-28 13:36:28 +0530

    tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -17,3 +17,11 @@ button = tk.Button(master, text="", width=3, height=1, command=lambda
 i=i: self.on_click(i))
 button.grid(row=i // 3, column=i % 3)
 self.buttons.append(button)
+
+# CREATE MENU
+menubar = tk.Menu(master)
+filemenu = tk.Menu(menubar, tearoff=0)
+filemenu.add_command(label="New Game", command=self.new_game)
+filemenu.add_command(label="Quit", command=self.quit)
+menubar.add_cascade(label="Tic-Tac-Toe", menu=filemenu)
+master.config(menu=menubar)
